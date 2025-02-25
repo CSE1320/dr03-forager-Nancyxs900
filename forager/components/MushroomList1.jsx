91c30075cd6baa4ea1b200a3a1b3e0cda927bacc
Nancyxs900 <107282510+Nancyxs900@users.noreply.github.com>
2025-02-25 06:27:01 +0000

    changed list

--- forager/components/MushroomList1.jsx
+++ forager/components/MushroomList1.jsx
@@ -11,13 +11,13 @@ const mushrooms = [
 export default function MushroomList1() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-200">
-      <ul className="flex flex-wrap gap-5 row-gap-9 justify-start max-w-[364px] h-[380px] list-none p-0">
+      <ul className="flex flex-wrap gap-x-5 gap-y-9 justify-between max-w-[364px] h-[380px] list-none p-0">
         {mushrooms.map((mushroom, index) => (
           <li key={index} className="relative w-[103px] h-[165px] flex-shrink-0 text-[#203B5F] text-center font-nunito text-[16.4px] font-semibold">
-            <div className="bg-white p-2 w-[103px] h-[165px] inline-block shadow-md">
+            <div className="bg-white p-2 w-[103px] h-[165px] inline-block shadow-md relative">
               <img src={`/icons/${mushroom.image}`} alt={`${mushroom.name} mushroom`} width={96} height={106} />
               {["Death Cap", "Destroying Angel", "False Death Cap"].includes(mushroom.name) && (
-                <div className="absolute bottom-[109px] left-[2px] w-[20px] h-[20px]">
+                <div className="absolute top-[-20px] left-2 w-[20px] h-[20px]">
                   <img src="/icons/icon_warning.svg" alt="Warning icon" className="w-1/2 h-auto" />
                 </div>
               )}
